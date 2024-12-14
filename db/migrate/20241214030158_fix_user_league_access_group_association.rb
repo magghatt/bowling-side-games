62e11816ab04a0a67565a628a088ec9f8d513c9f
Mike Tan <miketan.coderman@gmail.com>
2024-12-13 21:21:01 -0600

    fix lint style

--- db/migrate/20241214030158_fix_user_league_access_group_association.rb
+++ db/migrate/20241214030158_fix_user_league_access_group_association.rb
@@ -5,7 +5,7 @@ class FixUserLeagueAccessGroupAssociation < ActiveRecord::Migration[8.0]
 
     remove_index :user_league_access_groups, column: :access_groups_id
     remove_index :user_league_access_groups, column: :user_id
-    
+
     remove_column :user_league_access_groups, :access_groups_id, :integer
     remove_column :user_league_access_groups, :user_id, :integer
   end
