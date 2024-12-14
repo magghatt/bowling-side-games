34b429e5faa9b4fd9cbd845e7236a7ea24339ae2
Mike Tan <miketan.coderman@gmail.com>
2024-12-13 19:44:28 -0600

    clean up github check fails

--- db/migrate/20241213234936_create_user_league_accesses.rb
+++ db/migrate/20241213234936_create_user_league_accesses.rb
@@ -2,7 +2,7 @@ class CreateUserLeagueAccesses < ActiveRecord::Migration[8.0]
   def change
     create_table :user_league_access_groups do |t|
       t.belongs_to :user_league_access_groups
-      
+
       t.integer :user_id
       t.integer :league_id
       t.integer :access_group_id
